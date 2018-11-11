856fec0659f6f9e5219b61f44a204b07d1a554ba
lamp216 <martinslineu@linuxmail.org>
2018-11-11 20:45:13 +0100

    Chiron: Arrow-ify
    *Adapt device tree for arrow os

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_chiron.mk
+	$(LOCAL_DIR)/arrow_chiron.mk
--- arrow_chiron.mk
+++ arrow_chiron.mk
@@ -16,14 +16,24 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from chiron device
 $(call inherit-product, device/xiaomi/chiron/device.mk)
 
+# Inherit some common Arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := full_chiron
+PRODUCT_NAME := arrow_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
